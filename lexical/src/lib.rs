c411954108e408cbd1635fa70ac9620317a7f496
Sjors Holtrop <sjors@sholtrop.dev>
2021-11-09 13:39:01 +0100

    expand grammar

--- lexical/src/lib.rs
+++ lexical/src/lib.rs
@@ -1,32 +1,14 @@
-use pest::{error::Error as ParseError, Parser};
+use pest::{error::Error as ParseError, iterators::Pairs, Parser};
 use pest_derive::Parser;
+
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct LexicalParser;
 
-pub fn parse() -> Result<(), ParseError<Rule>> {
-    let pairs = LexicalParser::parse(
-        Rule::program,
-        "
-    int main(void) {
-        int a = 0, b;
-        return 0;
-    }",
-    )
-    .map_err(|err| {
+pub fn parse(input: &str) -> Result<Pairs<Rule>, ParseError<Rule>> {
+    let pairs = LexicalParser::parse(Rule::program, input).map_err(|err| {
         log::error!("{}", err);
         err
     })?;
-
-    // Because ident_list is silent, the iterator will contain idents
-    for pair in pairs {
-        // A pair is a combination of the rule which matched and a span of input
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", pair.as_span());
-        println!("Text:    {}", pair.as_str());
-        // println!("Miep:    {:?}", pair.tokens());
-        println!();
-    }
-
-    Ok(())
+    Ok(pairs)
 }
--- lexical/src/main.rs
+++ lexical/src/main.rs
@@ -2,9 +2,18 @@ mod lib;
 use general::logging;
 use lib::parse;
 
+use pest_ascii_tree::into_ascii_tree;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     logging::init_logger();
-    parse()?;
+    let result = parse(
+        "
+    int main(void) {
+        func(1, a, 3);
+    }",
+    )?;
+    let tree = into_ascii_tree(result)?;
+    log::info!("{}", tree);
     log::info!("Parsed successfully");
     Ok(())
 }
